30d6eeebf2fa370bc3db489f7e0e542d8cbaee5e
Wim <you@example.com>
2018-07-04 23:41:56 +0200

    work

---
+++ api/model/waitForMongo.js
@@ -0,0 +1,45 @@
+//var MongoClient = require('mongodb').MongoClient;
+var MONGO_OPTIONS = {
+  //server: {poolSize: 1},
+  //db: {readPreference: 'primary'}
+};
+
+function waitForMongo(mongoUrl, options, callback) {
+  if(typeof(options) == 'function') {
+    callback = options;
+    options = {};
+  }
+
+  options = options || {};
+  options.timeout = options.timeout || 1000 * 60 * 2; //2 minutes
+  var startedTime = Date.now();
+  var timeouted = false;
+
+  var timeoutHandler = setTimeout(function() {
+    timeouted = true;
+    callback(new Error('TIMEOUTED_WAIT_FOR_MONGO'));
+  }, options.timeout);
+
+  connectAgain();
+  function connectAgain() {
+      console.log('connect again');
+
+    //MongoClient.connect(mongoUrl, MONGO_OPTIONS, function(err, db) {
+        mongoose.connect(url);
+      if(timeouted) return;
+
+      if(err) {
+        console.log('wait-for-mongo: ' + err.message);
+        setTimeout(connectAgain, 2000);
+      } else {
+          console.log('connected2');
+        clearTimeout(timeoutHandler);
+        timeoutHandler = null;
+        //db.close();
+        callback();
+      }
+    });
+  }
+}
+
+module.exports = waitForMongo;
