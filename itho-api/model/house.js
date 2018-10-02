b7a529376ea1497434efea1eff23d8964b86bbfa
Wim <you@example.com>
2018-10-02 12:12:08 +0200

    removed the old itho app

--- itho-api/model/house.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var houseSchema = mongoose.Schema({
-    name: String,
-    ip: String,
-    parameters: [{ name: String, value: String }]
-});
-
-var House = module.exports = mongoose.model('House', houseSchema);
--- itho-api/model/stoveStatusEvent.js
+++
@@ -1,9 +0,0 @@
-console.log('compile stoveStatusEvent');
-var mongoose = require('mongoose');
-var options = { discriminatorKey: 'kind' };
-var Event = require('./event');
-
-var stoveStatusEventSchema = new mongoose.Schema({
-    level: Number
-}, options);
-var stoveStatusEvent = module.exports = Event.discriminator('StoveStatus', stoveStatusEventSchema);
