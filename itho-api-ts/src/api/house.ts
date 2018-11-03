92632a72a6598f981d36f9e49425418605e3f86b
Wim Philipsen <wim@zonneklaar-consulting.com>
2018-11-03 13:26:14 +0100

    Fix del2 (#11)
    * work on infra
    
    * also print remote name
    
    * cleaning, refactor
    
    * refactor
    
    * work
    
    * added state update using socket
    
    * cleanup
    
    * cleanup
    
    * typo
    
    * fix
    
    * f
    
    * work
    
    * w
    
    * work on local docker-compose setup, fi nginx
    
    * fix nginx
    
    * w
    
    * more api port
    
    * hide messages
    
    * update cluster
    
    * fix
    
    * improve state by trimming leading 0
    
    * work in infra
    
    * add delete
    
    * work on infra
    
    * add sec fixes

--- itho-api-ts/src/api/house.ts
+++ itho-api-ts/src/api/house.ts
@@ -17,6 +17,9 @@ export class HouseApi {
         router.get('/house/:name', (req: Request, res: Response, next: NextFunction) => {
             new HouseApi().getHouse(req, res, next);
         });
+        router.delete('/house/:name', (req: Request, res: Response, next: NextFunction) => {
+            new HouseApi().deleteHouse(req, res, next);
+        });
         router.get('/house/status/:name', (req: Request, res: Response, next: NextFunction) => {
             new HouseApi().getHouseState(req, res, next);
         });
@@ -55,9 +58,16 @@ export class HouseApi {
         }).catch(next);
     }
 
+    deleteHouse(req: Request, res: Response, next: NextFunction): any {
+        const name = req.params.name;
+        House.deleteOne({ name: name }).then(result => {
+            res.sendStatus(result.n > 0 ? 200 : 404);
+            next();
+        }).catch(next);
+    }
+
     getHouseState(req: Request, res: Response, next: NextFunction): any {
         const name = req.params.name;
-
         IthoEvent.find({ house: name }, undefined, { sort: { 'time': -1 }, limit: 1 }).then(events => {
             const state: IHouseState = new StateFactory().getState(events);
             res.json(state);
